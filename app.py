a047d03ab1045ea3fc0673e93c6314ad2c1e8d66
merlaannisaa <89918253+merlaannisaa@users.noreply.github.com>
2023-10-14 20:15:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def plot_stroke_risk(prediction):
     plt.title("Stroke Risk")
     st.pyplot()
 
+# Set page config
 st.set_page_config(layout="wide")
 
 # Muat model
@@ -34,39 +35,52 @@ st.title("Stroke Prediction App")
 # Input data
 st.subheader("Enter Patient Information")
 
-gender = st.radio("Gender", {"Female (0)": 0, "Male (1)": 1})
+gender = st.radio("Gender", ["Female", "Male"])
+gender = 0 if gender == "Female" else 1
 
 age = st.text_input("Age")
 
-hypertension = st.radio("Hypertension", {"No (0)": 0, "Yes (1)": 1})
+hypertension = st.radio("Hypertension", ["No", "Yes"])
+hypertension = 0 if hypertension == "No" else 1
 
-heart_disease = st.radio("Heart Disease", {"No (0)": 0, "Yes (1)": 1})
+heart_disease = st.radio("Heart Disease", ["No", "Yes"])
+heart_disease = 0 if heart_disease == "No" else 1
 
-ever_married = st.radio("Ever Married", {"No (0)": 0, "Yes (1)": 1})
+ever_married = st.radio("Ever Married", ["No", "Yes"])
+ever_married = 0 if ever_married == "No" else 1
 
-work_type_dict = {"Govt Job (0)": 0, "Never Worked (1)": 1, "Private (2)": 2, "Self-employed (3)": 3, "Children (4)": 4}
-work_type = st.radio("Work Type", work_type_dict)
+work_type_dict = {"Govt Job": 0, "Never Worked": 1, "Private": 2, "Self-employed": 3, "Children": 4}
+work_type = st.radio("Work Type", list(work_type_dict.keys()))
+work_type = work_type_dict[work_type]
 
-residence_type = st.radio("Residence Type", {"Rural (0)": 0, "Urban (1)": 1})
+residence_type = st.radio("Residence Type", ["Rural", "Urban"])
+residence_type = 0 if residence_type == "Rural" else 1
 
 avg_glucose_level = st.text_input("Average Glucose Level")
 
 bmi = st.text_input("BMI")
 
-smoking_status_dict = {"Unknown (0)": 0, "Formerly Smoked (1)": 1, "Never Smoked (2)": 2, "Smokes (3)": 3}
-smoking_status = st.radio("Smoking Status", smoking_status_dict)
+smoking_status_dict = {"Unknown": 0, "Formerly Smoked": 1, "Never Smoked": 2, "Smokes": 3}
+smoking_status = st.radio("Smoking Status", list(smoking_status_dict.keys()))
+smoking_status = smoking_status_dict[smoking_status]
 
 if st.button("Predict"):
-    input_data = [[gender, int(age), hypertension, heart_disease, ever_married, work_type, residence_type, float(avg_glucose_level), float(bmi), smoking_status]]
-    prediction = predict_stroke(model, input_data)
-    
-    st.write("## Prediction Result")
-    if prediction[0] == 1:
-        st.error("High risk of stroke!")
-    else:
-        st.success("Low risk of stroke!")
+    try:
+        age = int(age)
+        avg_glucose_level = float(avg_glucose_level)
+        bmi = float(bmi)
+        input_data = [[gender, age, hypertension, heart_disease, ever_married, work_type, residence_type, avg_glucose_level, bmi, smoking_status]]
+        prediction = predict_stroke(model, input_data)
+        
+        st.write("## Prediction Result")
+        if prediction[0] == 1:
+            st.error("High risk of stroke!")
+        else:
+            st.success("Low risk of stroke!")
 
-    plot_stroke_risk(prediction)
+        plot_stroke_risk(prediction)
+    except ValueError:
+        st.error("Invalid input. Please check the values you entered.")
 
 # Tambahkan kode berikut untuk meng-host aplikasi di Streamlit Sharing
 if __name__ == "__main__":
