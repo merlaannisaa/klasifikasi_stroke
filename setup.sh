47fa584fd34c5f0937dac7c318ded608e378a908
merlaannisaa <89918253+merlaannisaa@users.noreply.github.com>
2023-10-17 09:26:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,1 @@
-mkdir -p ~/.streamlit/                                               
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\   
-showPyplotGlobalUse = false\n\
-\n\                       
-" > ~/.streamlit/config.toml
+
