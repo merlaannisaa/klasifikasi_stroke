306a182cf5d9a8377cf69c089f9004aa456798f2
merlaannisaa <89918253+merlaannisaa@users.noreply.github.com>
2023-10-25 14:58:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ elif menu ==  "Klasifikasi":
                 input_data = file
                 
                 if st.sidebar.button("Predict"):
-                        threshold = 0.1
+                    threshold = 0.1
                         proba = model.predict_proba(input_data[:, 1])
                         prediction = (proba > threshold).astype(int)
                         
