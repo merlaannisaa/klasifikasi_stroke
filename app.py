2034599c7f3b5767b8e6060c0894d8adc41f2fba
merlaannisaa <89918253+merlaannisaa@users.noreply.github.com>
2023-10-25 15:37:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -244,17 +244,6 @@ elif menu ==  "Klasifikasi":
                         
                     file["Prediction"] = prediction
                     # file["Probabilitas"] = proba
-
-                    labels = ['Kelas 0', 'Kelas 1']
-                    fig1, ax1 = plt.subplots()
-                    sizes = file["Prediction"].value_counts()
-                    total_data = len(file)
-                    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
-                    ax1.axis('equal')  # Lingkaran yang sama
-                    st.write(f"Jumlah Data: {total_data}")
-                    st.write(f"Jumlah Data Terprediksi 0: {sizes[0]} ({percentages[0]:.1f}%)")
-                    st.write(f"Jumlah Data Terprediksi 1: {sizes[1]} ({percentages[1]:.1f}%)")
-                    st.pyplot(fig1)
                         
                     st.write("## Hasil Prediksi Pada Data")
                     st.write(file)
