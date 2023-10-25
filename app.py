2e39a201796240edb00794b31cbba07d7bd637e6
merlaannisaa <89918253+merlaannisaa@users.noreply.github.com>
2023-10-25 16:48:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -256,9 +256,9 @@ elif menu ==  "Klasifikasi":
                         st.markdown(href, unsafe_allow_html=True)
 
         else:
-            uploaded_file2 = st.sidebar.file_uploader("Upload File", type=["csv"])
+            uploaded_file = st.sidebar.file_uploader("Upload File", type=["csv"])
 
-            if uploaded_file2 is not None:
+            if uploaded_file is not None:
                 st.sidebar.write("Upload File Success")
                 file = pd.read_csv(uploaded_file)
                 file2 = file.copy()
