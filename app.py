146b536b60da89d8e1c1d41fd042e936bab9978f
merlaannisaa <89918253+merlaannisaa@users.noreply.github.com>
2023-10-25 16:05:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -256,3 +256,41 @@ elif menu ==  "Klasifikasi":
                         st.markdown(href, unsafe_allow_html=True)
 
         elif input_type == "File with Labels":
+            uploaded_file = st.sidebar.file_uploader("Upload File", type=["csv"])
+
+            if uploaded_file is not None:
+                st.sidebar.write("Upload File Success")
+                file = pd.read_csv(uploaded_file)
+                file2 = file.copy()
+                required_columns = ['gender', 'ever_married', 'Residence_type', 'work_type', 'smoking_status']
+                missing_columns = [col for col in required_columns if col not in file.columns]
+                if missing_columns :
+                    st.error(f"Kolom_kolom berikut tidak ditemukan dalam file: {', '.join(missing_columns)}'")
+                else:
+                    lab_enc = LabelEncoder()      
+                    lab_enc_data= file[required_columns]
+                    for x in lab_enc_data.columns:
+                        lab_enc_data[x]=lab_enc.fit_transform(lab_enc_data[x])
+                    for x in lab_enc_data.columns:
+                        file[x]=lab_enc_data[x]
+                    input_data = file
+
+                    x = input_data.drop(columns=['stroke']
+                    y = input_data['stroke']
+                    if st.sidebar.button("Predict"):
+    
+                        threshold = 0.1
+                        proba = model.predict_proba(x)[:, 1]
+                        prediction = (proba > threshold).astype(int)
+                        acc = accuracy_score(y, prediction)
+                            
+                        file2["Prediction"] = prediction
+                        # file2["Probabilitas"] = proba
+                            
+                        st.write("## Hasil Prediksi Pada Data") 
+                        st.write(file2)
+                              
+                        csv = file.to_csv(index=False)
+                        b64 = base64.b64encode(csv.encode()).decode()
+                        href = f'<a href="data:file/csv;base64,{b64}" download="hasil_prediksi.csv">Unduh Hasil Prediksi (CSV)</a>'
+                        st.markdown(href, unsafe_allow_html=True)
