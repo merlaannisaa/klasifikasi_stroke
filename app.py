ccfe6cbb466c26c34924e7bc30569687bc4d233a
merlaannisaa <89918253+merlaannisaa@users.noreply.github.com>
2023-10-25 19:30:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -113,9 +113,9 @@ if menu == "Visualisasi":
 
     plt.figure(figsize=(20,12))
     plt.bar(metrics, values)
-    plt.title("Evaluation Metrics on Test Data", font_size=15)
-    plt.xlabel("Metrics", font_size=15 )
-    plt.ylabel("Values", font_size=15)
+    plt.title("Evaluation Metrics on Test Data", fontsize=15)
+    plt.xlabel("Metrics", fontsize=15 )
+    plt.ylabel("Values", fontsize=15)
     st.pyplot(plt)
 
     fpr, tpr, thresholds = roc_curve(y_test, y_pred)
