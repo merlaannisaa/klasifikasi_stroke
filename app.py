843a96261a2aaf73ad9e805cd7eb4ed62de06ebf
merlaannisaa <89918253+merlaannisaa@users.noreply.github.com>
2023-10-25 15:28:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -247,6 +247,7 @@ elif menu ==  "Klasifikasi":
 
                     labels = ['Kelas 0', 'Kelas 1']
                     fig1, ax1 = plt.subplots()
+                    sizes = file["Prediction"].value_counts()
                     ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
                     ax1.axis('equal')  # Lingkaran yang sama
                     st.write(f"Jumlah Data: {total_data}")
